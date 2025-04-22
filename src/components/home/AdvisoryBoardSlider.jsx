b2046fd544f554fd6d7397172154686e077e97e8
muhammadhnhtechsolutions <muhammad@hnhtechsolutions.com>
2025-04-23 01:04:40 +0500

    color merge

--- src/components/home/AdvisoryBoardSlider.jsx
+++ src/components/home/AdvisoryBoardSlider.jsx
@@ -251,8 +251,7 @@ const AdvisoryBoardSlider = ({ heading, text, advisors }) => {
                         ))}
                     </div>
 
-                    <div className="relative overflow-hidden rounded-xl bg-[#F5FAFA] h-[400px] md:h-[350px]
-                    md:p-16 p-4 flex justify-center items-center"
+                    <div className="relative overflow-hidden rounded-xl bg-[#F5FAFA] h-[400px] md:h-[350px] md:p-16 p-4 flex justify-center items-center"
                         style={{
                             boxShadow: '0 0 20px 6px #ff0000',
                         }}
